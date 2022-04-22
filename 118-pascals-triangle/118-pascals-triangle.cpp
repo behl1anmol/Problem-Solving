876adeab3ecc4374f27e8712e205d5263cb4730b
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-04-22 19:20:04 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (33.66%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -3,24 +3,19 @@ public:
     vector<vector<int>> generate(int numRows) {
        if(numRows==1){ return {{1}}; }
         if(numRows==2){ return {{1},{1,1}}; }
-       vector<vector<int>> ans{{1},{1,1}};
+       vector<vector<int>> ans{{1}};
         int i,j;
-        for(i=0;i<numRows-2;i++){
+        for(i=1;i<numRows;i++){
+            vector<int> temp;
+            int sum;
+            temp.push_back(1);
             
-            vector<int>arr;
-            int n = ans.size();
-            int m = ans[n-1].size();
-            vector<int> temp(ans[n-1]);
-            int sum = ans[n-1][0];    
-            
-            arr.push_back(1);
-            for(j=0;j<m-1;j++){
-                sum = sum + temp[j+1];
-                arr.push_back(sum);
-                sum = sum-temp[j];
+            for(j=1;j<i;j++){
+                sum = ans[i-1][j] + ans[i-1][j-1];
+                temp.push_back(sum);
             }
-            arr.push_back(1);
-            ans.push_back(arr);
+            temp.push_back(1);
+            ans.push_back(temp);
             
         }
             
