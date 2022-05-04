8eda931127e26e47c8b2794b7782ade0d0ca9570
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-05-04 16:41:57 +0530

    Time: 21 ms (11.71%), Space: 7.5 MB (74.73%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode *getCycleBegin(ListNode *node, ListNode* head){
+        
+        ListNode *p1 = node->next;
+        ListNode *p2 = node;
+        int k = 1;
+        while(p1!=p2){
+            p1 = p1->next;
+            cout<<"counting nodes";
+            k++;
+        }
+        cout<<k;
+        
+        p1 = p2 = head;
+        
+        for(int i=0;i<k;i++){
+            p1 = p1->next;
+        }
+        
+        while(p1!=p2){
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        
+        return p1;
+        
+        
+        
+    }
+    
+    ListNode *detectCycle(ListNode *head) {
+        
+        if(head == NULL || head->next == NULL) return NULL;
+        
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast==slow) break;
+            cout<<"checking loop"; 
+        }
+        if(fast==NULL || fast->next==NULL) return NULL;
+        
+        return getCycleBegin(fast, head);
+    }
+};
