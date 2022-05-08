9638d6369d8b9326c665a168d5d0e95e34399520
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-05-08 15:53:59 +0530

    Time: 0 ms (100.00%), Space: 12 MB (77.35%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(!root) return {};
+        vector<vector<int>> result;
+        if(!root->left && !root->right){
+            vector<int> ans;
+            ans.push_back(root->val);
+            result.push_back(ans);
+            return result;
+        }
+        stack<TreeNode*> s1;
+        stack<TreeNode*>s2;
+        
+        s1.push(root);
+        
+        while(!s1.empty() || !s2.empty()){
+            vector<int>ans;
+            ans.clear();
+            if(!s1.empty()){
+            while(!s1.empty()){
+                    TreeNode* top = s1.top();
+                    s1.pop();
+                    ans.push_back(top->val);
+                    if(top->left) s2.push(top->left);
+                    if(top->right) s2.push(top->right);
+                }
+            }
+            else{
+                while(!s2.empty()){
+                    TreeNode* top = s2.top();
+                    s2.pop();
+                    ans.push_back(top->val);
+                    if(top->right) s1.push(top->right);
+                    if(top->left) s1.push(top->left);
+                }
+            }
+            result.push_back(ans);
+        }
+        return result;
+    }
+};
