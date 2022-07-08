0663c0f108a18c0eda0398d059c8a5ebefe6e805
Anmol Behl <behl1anmol@gmail.com>
2022-07-08 04:00:00 +0000

    Added solution - LeetHub

---
+++ Stock Buy and Sell - GFG/stock-buy-and-sell.cpp
@@ -0,0 +1,73 @@
+class Solution{
+public:
+    //Function to find the days of buying and selling stock for max profit.
+    vector<vector<int> > stockBuySell(vector<int> A, int n){
+        // code here
+        
+        vector<vector<int>> ans;
+
+        int buy = 0;
+        int sell = 0;
+        
+        int temp=buy;
+        
+        while(sell<n){
+            if(A[sell+1]>=A[temp]){
+                sell+=1;
+                temp+=1;
+            }
+            else{
+                //cout<<buy<<' '<<sell<<'\n';
+                ans.push_back({buy,sell});
+                sell = sell+1;
+                buy=sell;
+                temp=buy;
+            }
+        }
+        
+        
+        return ans;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int check(vector<vector<int>> ans, vector<int> A, int p)
+{
+    int c = 0;
+    for(int i=0; i<ans.size(); i++)
+        c += A[ans[i][1]]-A[ans[i][0]];
+    return (c==p) ? 1 : 0;
+}
+
+int main()
+{   
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A(n);
+        for (int i=0; i<n; ++i){
+            cin>>A[i];
+        }
+        Solution ob;
+        vector<vector<int> > ans = ob.stockBuySell(A, n);
+        int p = 0;
+        for(int i=0; i<n-1; i++)
+        {
+            int x = A[i+1]-A[i];
+            if(x>0)
+                p += x;
+        }
+        if(ans.size()==0)
+            cout<<"No Profit";
+        else{
+            cout<<check(ans,A,p);
+        }cout<<endl;
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
