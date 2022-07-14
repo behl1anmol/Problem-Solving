88fd1c8e2b5059f3c35f4ff149e06e0117475d49
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-07-14 17:52:58 +0530

    Time: 3 ms (47.21%), Space: 5.9 MB (43.20%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
+    
+    double binaryExponentiation(double a, long long int n){
+    if(n==0)
+        return 1;
+
+    double res = binaryExponentiation(a,n/2);
+
+    if(n%2!=0)
+        return res*res*a;
+    else
+        return res*res;
+            
+}
+    
+    
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(n<0) return (1/binaryExponentiation(x, abs(n)));
+        return binaryExponentiation(x,n);
     }
 };
