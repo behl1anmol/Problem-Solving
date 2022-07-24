b741aaa838a30723a79c8be4c248fb9e56d56a51
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-07-24 21:00:07 +0530

    Time: 69 ms (5.40%), Space: 10.3 MB (48.65%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.c
@@ -0,0 +1,22 @@
+
+
+int singleNonDuplicate(int* nums, int numsSize){
+    
+    int low = 0;
+    int high = numsSize-2;
+    int mid = 0;
+    while(low<=high){
+       mid = (low+high)/2;
+        
+        if(mid%2!=0){
+            printf("%d %d\n",mid, nums[mid]);
+            if(nums[mid] == nums[mid+1]) high = mid-1;
+            else low = mid+1;
+        }
+        else{
+            if(nums[mid] != nums[mid+1]) high = mid-1;
+            else low = mid+1;
+        }
+    }
+    return nums[low];
+}
