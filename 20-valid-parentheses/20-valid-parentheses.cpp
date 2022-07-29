af816ad887c3e92ae87c731352a2802d164c767f
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-07-29 19:49:31 +0530

    Time: 4 ms (31.79%), Space: 6.1 MB (95.98%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        int n = s.length();
+        cout<<n;
+        
+        if(n==1) return false;
+        if(s[0]==')' || s[0]=='}' || s[0]==']') return false;
+        if(s[n-1]=='(' || s[n-1]=='[' || s[n-1]=='{') return false;
+        
+        stack<char> st;
+        
+        for(int i=0;i<n;i++){
+            
+            switch(s[i]){
+                    
+                case '(':
+                    st.push('(');
+                    break;
+                case '{':
+                    st.push('{');
+                    break;
+                case '[':
+                    st.push('[');
+                    break;
+                case '}':
+                    if(st.empty() || st.top()!='{') return false;
+                    st.pop();
+                    break;
+                case ']':
+                    if(st.empty() || st.top()!='[') return false;
+                    st.pop();
+                    break;
+                case ')':
+                    if(st.empty() || st.top()!='(') return false;
+                    st.pop();
+                    break;
+            }
+            
+        }
+        
+        if(!st.empty()) return false;
+        return true;
+        
+        
+    }
+};
