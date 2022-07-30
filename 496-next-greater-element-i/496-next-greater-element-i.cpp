de560d8bfc1c30c1b638facc4373bbd3592a804d
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-07-30 16:11:20 +0530

    Time: 14 ms (35.69%), Space: 8.9 MB (33.08%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st;
-        stack<int> q;
-        
         int n1 = nums1.size();
         int n2 = nums2.size();
         
-        for(int i=0;i<n2;i++){
+        stack<int> st;
+        map<int,int>nge;
+        
+        for(int i=n2-1;i>=0;i--){
+            
+            while(!st.empty() && st.top()<nums2[i]) st.pop();
+            
+            if(!st.empty()) nge[nums2[i]] = st.top();
+            else nge[nums2[i]] = -1;
+            
             st.push(nums2[i]);
+            
         }
         
+        vector<int> ans;
         
-        vector<int> ans(n1,-1);
-        
+        /*for(auto i:nge){
+            cout<<i.first<<" "<<i.second<<"\n";
+        }*/
         
         for(int i=0;i<n1;i++){
-            
-            int front = st.top();
-
-            while(front!=nums1[i]){
-                q.push(front);
-                st.pop();
-                front = st.top();
-
-            }
-
-            while(!q.empty()){
-                int qf = q.top();
-                if(qf>nums1[i] && ans[i]==-1){
-                    ans[i] = qf;
-                }
-                st.push(qf);
-                q.pop();
-            }
-            
-            
+            ans.push_back(nge[nums1[i]]);
         }
+        
+        
+
+        
+        
         return ans;
     }
 };
