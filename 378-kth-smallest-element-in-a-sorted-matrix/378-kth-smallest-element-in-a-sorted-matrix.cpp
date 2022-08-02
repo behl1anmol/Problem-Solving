d19424322c921f522f7f4dfefc325c8cfaefc66c
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-08-02 10:30:10 +0530

    Time: 41 ms (77.93%), Space: 13.1 MB (92.86%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+    
+    int count_smaller_than_mid(vector<vector<int>> &matrix, int mid){
+        int cnt = 0;
+        
+        for(int i=0;i<matrix.size();i++){
+            cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+        }
+        return cnt;
+    }
+    
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
+        //Approach 2 Binary Search
+        int n = matrix.size();
+        int left = matrix[0][0];
+        int right = matrix[n-1][n-1];
+        
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            
+            if(count_smaller_than_mid(matrix,mid) < k) left = mid+1;
+            else right = mid-1;
+        }
+        
+        return left;
+        
+    
+        /*
+        Approach 1 
         vector<int> arr;
         
         for(int i=0;i<matrix.size();i++){
@@ -11,7 +39,7 @@ public:
         sort(arr.begin(),arr.end());
         
 
-        return arr[k-1];
+        return arr[k-1];*/
         
     }
 };
