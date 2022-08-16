762c2c515ec6246e1550cd9c50d9589c1843553d
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-08-16 11:17:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (22.67%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -6,21 +6,19 @@ public:
     int firstBadVersion(int n) {
         int l = 0;
         int r = n;
-        int ans = INT_MAX;
         
         while(l<=r){
             
             int mid = l+(r-l)/2;
             
             bool isBad = isBadVersion(mid);
-            if(isBad==true){
-                ans = min(ans,mid);
+            if(isBad){
                 r = mid-1;   
             }
             else l = mid+1;
             
             
         }
-        return ans;
+        return l;
     }
 };
