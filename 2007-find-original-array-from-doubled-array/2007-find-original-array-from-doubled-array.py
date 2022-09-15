734668e5aa6ccdb83f7cb40cf661e6eeddfaa8d2
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-09-15 14:35:41 +0530

    Time: 3367 ms (12.31%), Space: 32.8 MB (33.24%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -12,12 +12,10 @@ class Solution:
                 dic[i]+=1
             else:
                 dic[i] = 1
-        #print(dic)
+
         for i in changed:
             if dic[i]==0: continue
             if (2*i) not in dic.keys() or dic[2*i]==0: return []
-            
-            #print(i)
             ans.append(i)
             dic[i] -=1
             dic[2*i] -= 1
