c71d705907e4e23ea082a12b8c4545198603fd99
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-09-17 00:00:58 +0530

    Time: 893 ms (25.64%), Space: 120.1 MB (66.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int maximumScoreUtils(vector<int>& nums, vector<int>& multipliers, int i, int j, int n){
+    int maximumScoreUtils(vector<vector<int>>& dp, vector<int>& nums, vector<int>& multipliers, int i, int j, int n){
         
         if(j == multipliers.size()) return 0;
         
         if(dp[i][j]!=INT_MIN)
             return dp[i][j];
         
-           dp[i][j] = max(maximumScoreUtils(nums, multipliers, i+1, j+1, n) + (nums[i]*multipliers[j]) ,maximumScoreUtils(nums, multipliers, i, j+1,n) + (nums[(n-1)-(j-i)]*multipliers[j]));
+           dp[i][j] = max(maximumScoreUtils(dp,nums, multipliers, i+1, j+1, n) + (nums[i]*multipliers[j]) ,maximumScoreUtils(dp,nums, multipliers, i, j+1,n) + (nums[(n-1)-(j-i)]*multipliers[j]));
         return dp[i][j];
     }
     
@@ -16,9 +15,8 @@ public:
         
         
         int m = multipliers.size();
-        dp.resize(m + 1, vector<int>(m+ 1, INT_MIN));
+        vector<vector<int>>dp(m + 1, vector<int>(m+ 1, INT_MIN));
         
-        
-        return maximumScoreUtils(nums, multipliers, 0, 0, nums.size());
+        return maximumScoreUtils(dp,nums, multipliers, 0, 0, nums.size());
     }
 };
