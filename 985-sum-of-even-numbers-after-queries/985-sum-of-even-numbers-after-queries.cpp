7998f5886e94ae7f4ad8fde81634543d3f2b722b
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-09-21 18:01:20 +0530

    Time: 236 ms (27.18%), Space: 46.6 MB (25.00%) - LeetHub

---
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+    int calculate_sum(vector<int> nums){
+        
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0)
+                sum += nums[i];
+        }
+        return sum;
+    }
+    
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        
+        
+        int n = nums.size();
+        int q = queries.size();
+        vector<int> ans;
+        int sum = calculate_sum(nums);
+        
+        //val idx
+        for(int i=0; i<n; i++){
+            int val = queries[i][0], idx = queries[i][1];
+            int preVal = nums[idx];
+            
+            
+            nums[idx] += val;
+            
+            bool EvenBefore = (preVal %2)==0;
+            bool EvenNow = (nums[idx]%2==0);
+                
+            
+            if(EvenBefore && EvenNow){
+                sum = sum + val;
+                ans.push_back(sum);
+            }
+            else if (!EvenBefore && EvenNow){
+                sum = sum + nums[idx];
+                ans.push_back(sum);
+            }
+            else if(EvenBefore && !EvenNow){
+
+                sum = sum - preVal;
+                ans.push_back(sum);
+            }
+            else{
+                ans.push_back(sum);
+            }
+        }
+        
+        return ans;
+        
+        
+    }
+};
