b09169a851921cbbd049079324d0ad8ef70069a1
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-10-16 18:57:58 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.92%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     
+/*  TN = O(N) SN = O(N) + O(N) MEMONIASATION
+    
+    
     int sol(int n, vector<int>& dp){
         if(n == 1) return 1;
         if(n == 2) return 2;
@@ -15,5 +18,18 @@ public:
         vector<int> dp(n+1, -1);
         
         return sol(n, dp);
+    }
+*/
+    
+    int climbStairs(int n) {
+        vector<int> dp(n+2, -1);
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
