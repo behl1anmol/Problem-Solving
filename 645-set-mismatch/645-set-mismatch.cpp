2b32c7ecf21119c832fdd9ec4339e0c9437f3b04
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-10-23 14:32:52 +0530

    Time: 166 ms (14.84%), Space: 44.7 MB (8.73%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        
+        int n = nums.size();
+        if(n==2){
+            if(nums[0]==1) return {1,2};
+            return {2,1};
+        }
+        map<int,int>mp;
+        
+        set<int>st;
+        
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+            st.insert(nums[i]);
+        }
+        
+        int m = 1;
+        for(auto i:st){
+            if(i != m)break;
+            m++;
+        }
+        
+        //cout<<m<<endl<<n-1;
+        vector<int>ans(2);
+        
+        ans[1] = m;
+        int ele = nums[0];
+        int idx = 0;
+        
+        for(int i=0;i<n;i++){
+            if(mp[nums[i]]>1){
+                ans[0] = nums[i];
+                idx = i;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
