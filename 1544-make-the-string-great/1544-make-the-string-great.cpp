e666877bd9e46426d8e03eca4f4c01d0dda052fc
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-11-08 10:30:14 +0530

    Time: 3 ms (82.74%), Space: 6.3 MB (65.67%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -7,12 +7,8 @@ public:
         
         while(flag != 0){
             flag = 0;
-            //cout<<"here\n";
-            //cout<<ans.size()<<endl;
             for(int i=0;ans.size()>1 && i<=ans.size()-2;i++){
-                //cout<<ans<<endl;
                 if(ans[i] != ans[i+1] && (ans[i] == tolower(ans[i+1]) || ans[i] == toupper(ans[i+1]))){
-                    //cout<<ans[i]<<" "<<ans[i+1]<<endl;
                     ans.erase(ans.begin()+i);
                     ans.erase(ans.begin()+i);
                     flag = 1;
