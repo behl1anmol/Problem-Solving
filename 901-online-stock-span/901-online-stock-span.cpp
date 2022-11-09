f543058a250934c7f7a349c7fe19bc5e30f5f70d
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2022-11-09 11:08:04 +0530

    Time: 338 ms (76.39%), Space: 89.7 MB (5.16%) - LeetHub

---
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -0,0 +1,39 @@
+class StockSpanner {
+public:
+    unordered_map<int,int> cache;
+    vector<int> stock;
+    int sz;
+    StockSpanner() {
+        sz= 0;
+    }
+    
+    int next(int price) {
+        stock.push_back(price);
+        sz = stock.size();
+        int span = 1;
+        if(sz==1 || (sz>1 && stock[sz-2] > stock[sz-1])){
+            cache[price] = span;
+            return span;
+        }
+
+
+        //if more than one stock
+        for(int i=sz-2;i>=0;i--){
+            if(stock[i] > price) break;
+            else{
+                span += cache[stock[i]];
+                i -= (cache[stock[i]]-1);
+                //cout<<i<<endl;
+            }
+        }
+        cache[price] = span;
+        return span;
+
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
