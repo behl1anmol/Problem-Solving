8e291cfc351ff59211ed27153b53d1f43becdb53
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2023-01-19 11:14:42 +0530

    Time: 12 ms (83.98%), Space: 8.5 MB (6.29%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.c
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.c
@@ -5,22 +5,13 @@
  *     struct ListNode *next;
  * };
  */
- void _removeElements(struct ListNode* head, struct ListNode* prev, int val){
-    if(head == NULL) return;
-    
-    if(head->val==val)
-        prev->next = head->next;
-    else
-        prev = head;
-    _removeElements(head->next, prev, val);    
-}
 struct ListNode* removeElements(struct ListNode* head, int val){
-    if(!head) return head;
+    
     while(head && head->val == val){
         head = head->next;
     }
-    _removeElements(head, NULL, val);
+    if(!head) return NULL;
     
+    head->next = removeElements(head->next, val);
     return head;
-    
 }
