49a516c5351242bfc7beb41d46d084a3f1cddcf5
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2023-01-30 10:52:47 +0530

    Time: 27 ms (87.50%), Space: 13.8 MB (62.50%) - LeetHub

---
+++ 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.py
@@ -0,0 +1,15 @@
+class Solution:
+    def monkeyMove(self, n: int) -> int:
+        mod = 1000000007
+        def exponentiation(base, exponent):
+            if exponent == 0:
+                return 1%mod
+            elif exponent == 1:
+                return base%mod
+            elif exponent%2==0:
+                hexp = ((exponent%mod)//2)%mod
+                hres = exponentiation(base,hexp)
+                return (hres%mod * hres%mod)%mod
+            else:
+                return base*exponentiation(base,exponent-1)
+        return (exponentiation(2,n)-2)%mod
