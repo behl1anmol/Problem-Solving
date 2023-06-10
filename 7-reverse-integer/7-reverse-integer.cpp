f593c5dc171776749ffc6dd1b362cb63556318a6
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2023-06-10 15:22:15 +0530

    Time: 7 ms (17.36%), Space: 6 MB (16.12%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,11 +1,15 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        if x < 0:
-            x = abs(x)
-            s = str(x)
-            x = -1 * int(s[-1::-1])
-        else:
-            s = str(x)
-            x = int(s[-1::-1])
-        return x if (x >= -2**31) and (x <= (2**31)-1) else 0
+class Solution {
+public:
+    int reverse(int x) {
+        long long int num = x;
+        long long int ans = 0;
         
+        while(num){
+            ans = ans*10 + num%10;
+            num = num/10;
+        }
+        
+        if(ans < INT_MIN || ans > INT_MAX) return 0;
+        return ans;
+    }
+};
