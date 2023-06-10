208b01d227b6347937031b856e8a712069a8c71f
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2023-06-10 09:16:46 +0000

    Geeks-for-Geeks (Count-Digits)

---
+++ count-digits/count-digits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        int ans = 0;
+        int num = N;
+        
+        while(num){
+            int rem = num%10;
+            if(rem!=0){
+                ans = N%rem==0?ans+1:ans;
+            }
+            num = num/10;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
