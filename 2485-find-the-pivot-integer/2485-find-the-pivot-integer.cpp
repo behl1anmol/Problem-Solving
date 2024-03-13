83352e423218bf07e7d79d5f6052384bd800faf1
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2024-03-13 10:03:43 +0530

    Time: 0 ms (100%), Space: 7.2 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
-    int pivotInteger(int n) {
+
+    //another good approach is to use simple maths.
+    //mathematically it can be proven that sum of all integers upto n is pivot*pivot
+
+    int pivotInteger(int n){
+        int sum = n*(n+1)/2;
+        int pivot= sqrt(sum);
+
+        return pivot * pivot == sum ? pivot : -1; 
+    }
+
+    int _pivotInteger(int n) {
         int leftSum[n+1], rightSum[n+1];
         int sumTillNow = 0;
 
