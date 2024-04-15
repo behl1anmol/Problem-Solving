6980abd26d3d297eb7cd54d4fa6b42e65da001cf
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2024-04-16 00:02:33 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.37%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,44 +11,23 @@
  */
 class Solution {
 public:
-    int tree_height(TreeNode* root) {
-        if (root == NULL)
-            return 0;
-        else {
-            int left_height = tree_height(root->left);
-            int right_height = tree_height(root->right);
-            return std::max(left_height, right_height) + 1;
-        }
-    }
-
     int _sumNumbers(TreeNode* root, string num){
         
-        //num = num + root->val*pow(10,h);
         num = num + to_string(root->val);
-        //cout<<num<<endl;
 
         if(!root->left && !root->right){
-            //reverse(num.begin(), num.end());
             int numi = stoi(num); 
             return numi;
         }
         
         int left = 0;
         left = root->left?_sumNumbers(root->left, num):left;
-        cout<<"left:"<<left;
         int right = 0;
         right = root->right?_sumNumbers(root->right, num):right;
-        cout<<" right:"<<right<<endl;
         return  left+right;  
     }
     int sumNumbers(TreeNode* root) {
-        int height = tree_height(root);
-        cout<<height<<endl;
-        if(height == 0) return 0;
-        if(height == 1) return root->val;
-
-        stack<int> st;
-
+        if(root == NULL) return 0;
         return _sumNumbers(root, "");
 
 
