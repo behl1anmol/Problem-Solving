272c512d430aa00f25b6cf6d87c0f951b6db56c1
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2024-06-02 15:27:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void CombinationalSum(int idx, int sum, vector<int>& ds, int k,
+                          vector<vector<int>>& ans) {
+        if(sum == 0 && ds.size() == k){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i = idx; i< 9; i++){
+
+            if(i > sum || ds.size() > k) break;
+
+            ds.push_back(i);
+            CombinationalSum(i+1, sum-i,ds,k, ans);
+            ds.pop_back();
+
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        CombinationalSum(1, n,ds,k,ans );
+
+        return ans;
+    }
+};
