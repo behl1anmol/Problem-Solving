af3ea3664cb00c39b53233d10bf855c5cf087975
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2025-01-23 12:05:47 +0530

    Time: 5 ms (89.28%), Space: 43.5 MB (43.95%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        var set = new HashSet<char>();
+var n = s.Length;
+var ans = 0;
+var windowStart = 0;
+
+
+for (var i = 0; i < n; i++)
+{
+    if (set.Contains(s[i]))
+    {
+        while (s[windowStart] != s[i])
+        {
+            set.Remove(s[windowStart]);
+            windowStart++;
+        }
+        windowStart++;
+    }
+    else
+    {
+        set.Add(s[i]);
+        ans = Math.Max(ans, i - windowStart + 1);
+    }
+
+}
+return ans;
+    }
+}
