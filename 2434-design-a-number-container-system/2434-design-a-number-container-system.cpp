5d538c4693ecb03f353e78a9d2f64292adcbb6fe
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2025-02-09 00:48:26 +0530

    Time: 171 ms (6.88%), Space: 195.2 MB (78.13%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -1,28 +1,36 @@
 class NumberContainers {
 public:
-    map<int, int> mapp;
-    map<int, set<int>> m;
-    NumberContainers() {}
-    void change(int i, int num) {
-        if (mapp.find(i) != mapp.end()) {
-            int o = mapp[i];
-            if (o == num)
+    map<int, set<int>> mpq;
+    map<int,int> mp;
+    NumberContainers() {
+        
+    }
+
+     void change(int index, int number) {
+        
+        if(mp.find(index) != mp.end())
+        {
+            int prevNumber = mp[index];
+            if(prevNumber == number)
                 return;
-            if (m.find(o) != m.end()) {
-                m[o].erase(i);
-                if (m[o].empty())
-                    m.erase(o);
+            if(mpq.find(prevNumber) != mpq.end())
+            {
+                mpq[prevNumber].erase(index);
+                if(mpq[prevNumber].empty()) mpq.erase(prevNumber);
             }
         }
-        mapp[i] = num;
-        m[num].insert(i);
+        mp[index] = number;
+        mpq[number].insert(index);
     }
-    int find(int num) {
-        if (m.find(num) != m.end() && !m[num].empty())
-            return *(m[num].begin());
+    
+    int find(int number) {
+        if(mpq.find(number) != mpq.end() && !mpq[number].empty())
+            return *(mpq[number].begin());
         return -1;
     }
 };
+    
+
 /**
  * Your NumberContainers object will be instantiated and called as such:
  * NumberContainers* obj = new NumberContainers();
