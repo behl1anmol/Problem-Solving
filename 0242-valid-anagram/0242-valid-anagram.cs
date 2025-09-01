59b9cfae78358e7afda949efc8d6537c9e93b09f
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2025-09-01 12:43:04 +0530

    Time: 7 ms (36.76%), Space: 44.1 MB (38.1%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        Dictionary<char, int> dict= new Dictionary<char, int>();
+
+        int n1 = s.Count();
+        int n2 = t.Count();
+
+        if(n1 != n2)
+        {
+            return false;
+        }
+        else
+        {
+            for(int i=0; i< n1; i++)
+            {
+                if(dict.ContainsKey(s[i])) dict[s[i]]++;
+                else dict[s[i]] = 1;
+            }
+
+            foreach(var i in t)
+            {
+                if(!dict.ContainsKey(i)) return false;
+                else dict[i]--;
+            }
+
+            if(dict.Any(d => d.Value != 0)) return false;
+            return true;
+        }
+    }
+}
