63ada9befd4280ef2f17bf796fc81c91f1dfabf6
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2025-09-01 15:32:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cs
+++ 0049-group-anagrams/0049-group-anagrams.cs
@@ -5,7 +5,8 @@ public class Solution {
         return new string(s.OrderBy(c=>c).ToArray());
     }
 
-    public IList<IList<string>> GroupAnagrams(string[] strs) 
+    // this is an O(N*KlogK) solution where k is largest string length
+    public IList<IList<string>> GroupAnagramsUnoptimized(string[] strs) 
     {
         var newStrs = new List<string>(); 
         foreach(var s in strs)
@@ -26,5 +27,25 @@ public class Solution {
             ans.Add(value);
         }
         return ans;
+    }
+
+    //we can do it in O(n*k)
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+        foreach(var str in strs)
+        {
+            int[] count = new int[26];
+            foreach(var c in str)
+                count[c - 'a']++;
+            
+            string key = string.Join("", count);
+             if(!dict.ContainsKey(key))
+                dict[key] = new List<string>();
+            
+            dict[key].Add(str);
+        }
+        return new List<IList<string>>(dict.Values);
     }
 }
