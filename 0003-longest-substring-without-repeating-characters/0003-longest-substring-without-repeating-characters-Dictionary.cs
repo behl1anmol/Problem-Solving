37e5d9c1ef45b491363f13e4bcb6e3ea757c57d3
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2025-10-08 15:14:31 +0530

    Time: 4 ms (95.47%), Space: 44.7 MB (27.82%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters-Dictionary.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int i = 0;
+        var dict = new Dictionary<char,int>();
+        int len = 0;
+
+        for(int j = 0; j <s.Length ; j++)
+        {
+            if(!dict.ContainsKey(s[j])) dict.Add(s[j],j);
+            else
+            { 
+                i = Math.Max(dict[s[j]] + 1, i);
+                dict[s[j]] = j; 
+            }
+            len = Math.Max((j-i)+1, len);
+        }
+
+        return len;
+    }
+}
