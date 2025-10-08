9be0d734dca67b1a5ee608d06abcce3955f20221
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2025-10-08 11:32:08 +0530

    Time: 2 ms (59.9%), Space: 49 MB (38.3%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,19 @@
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int,int> dict = new Dictionary<int,int>();
+
+        for(int i=0; i<nums.Count(); i++)
+        {
+            int rem = target-nums[i];
+            if(dict.ContainsKey(rem))
+            {
+                return [i, dict[rem]];
+            }
+            dict[nums[i]] = i;
+        }
+
+        return [];
+    }
+}
