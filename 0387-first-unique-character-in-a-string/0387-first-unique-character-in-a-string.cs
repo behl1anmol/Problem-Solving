e97d3094918e3abf28d996e8a45b2f7df075a8f4
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2025-11-04 09:43:44 +0530

    Time: 26 ms (17.15%), Space: 49.5 MB (27.72%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int FirstUniqChar(string s) {
+        var hash = new Dictionary<char, int>();
+        var ct = new Dictionary<char, int>();
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(!hash.ContainsKey(s[i]))
+            {
+                hash[s[i]] = i;
+            }
+            if(!ct.ContainsKey(s[i]))
+                ct[s[i]] = 1;
+            else
+                ct[s[i]]+=1;
+        }
+        
+        int res = s.Length+1;
+        foreach(var (key,val) in ct)
+        {
+            if(val == 1)
+            {
+                res = Math.Min(res, hash[key]);
+            }
+        }
+
+        return res == s.Length+1 ? -1:res;
+    }
+}
